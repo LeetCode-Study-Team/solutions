07d35a8742bbf359275672dc0048cc836eb86855
Yoo Chul Kim <yk41@st-andrews.ac.uk>
2021-09-14 12:01:02 +0100

    28번 문제 풀이 완료

--- src/yoochulkim/S0028.java
+++ src/yoochulkim/S0028.java
@@ -10,18 +10,33 @@ public class S0028 {
         String haystack2 = "aaaaa", needle2 = "bba";
         String haystack3 = "", needle3 = "";
 
-        System.out.println(strStr(haystack1, needle1)); // 2
-        System.out.println(strStr(haystack2, needle2)); // -1
-        System.out.println(strStr(haystack3, needle3)); // 0
+        System.out.println(strStr1(haystack1, needle1)); // 2
+        System.out.println(strStr1(haystack2, needle2)); // -1
+        System.out.println(strStr1(haystack3, needle3)); // 0
     }
 
+    // 이렇게 해되 되나...?
+    public static int strStr1(String haystack, String needle) {
+        return haystack.indexOf(needle);
+    }
 
-    public static int strStr(String haystack, String needle) {
-        if (needle.isEmpty()) { // 아무 값이 없을때 0 리턴
+   /* public static int strStr2(String haystack, String needle) {
+        if (needle.length() > haystack.length()) { // needle 길이가 더 클 경우 체크
+            return -1;
+        } else if (needle.isEmpty()) { // needle 이 비어있으면 무조건 haystack의 포지션 0으로 간주
             return 0;
         }
 
+        int iterationNumber = calculateIterationNumber(haystack.length(), needle.length());
+
+        for (int i = 0; i < iterationNumber; i++) { // 7 3
+            String test = haystack.substring(i, i + needle.length());
+            if (test.equals(needle)) {
+                return i; // haystack 에서 needle 시작점 리턴
+            }
+        }
 
         return -1;
-    }
+    }*/
+
 }
